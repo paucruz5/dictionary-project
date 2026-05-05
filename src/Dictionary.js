20e606c0760262ad0cc46be4bc242b0b72511924
Pau Cruz <paulinamc_micki@hotmail.com>
2026-05-05 14:53:35 -0600

    added api url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=8ct2716ea6f8a04o8535eed14cbdd63a`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
