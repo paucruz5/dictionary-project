70835a0436dd96ef909546ed06c22d234d2770f9
Pau Cruz <paulinamc_micki@hotmail.com>
2026-05-06 13:26:23 -0600

    Added synonyms

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello World</header>
+        <header className="App-header">The Meaning Archive</header>
         <main>
           <Dictionary />
         </main>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,14 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>
-        {props.meaning.definition}
-        <br />
-        <em>{props.meaning.example}</em>
-      </p>
+      <strong>Definition:</strong> {props.meaning.definition}
+      <br />
+      <strong>Example:</strong> <em>{props.meaning.example}</em>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
