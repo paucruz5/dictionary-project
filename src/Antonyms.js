f6fa9edaf119bd0703aad27307317d11204313df
Pau Cruz <paulinamc_micki@hotmail.com>
2026-05-06 14:28:53 -0600

    Added antonyms

---
+++ src/Antonyms.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Antonyms(props) {
+  if (props.antonyms) {
+    return (
+      <div className="Antonyms">
+        <strong>Antonyms:</strong>
+        {props.antonyms.map(function (antonym, index) {
+          return (
+            <ul key={index}>
+              <li>{antonym}</li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,27 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import Antonyms from "./Antonyms";
 
 export default function Meaning(props) {
+  function example() {
+    if (props.meaning.example) {
+      return (
+        <p>
+          <strong>Example:</strong> {props.meaning.example}
+        </p>
+      );
+    } else {
+      return null;
+    }
+  }
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <strong>Definition:</strong> {props.meaning.definition}
       <br />
-      <strong>Example:</strong> <em>{props.meaning.example}</em>
+      <em>{example()}</em>
       <Synonyms synonyms={props.meaning.synonyms} />
+      <Antonyms antonyms={props.meaning.antonyms} />
     </div>
   );
 }
